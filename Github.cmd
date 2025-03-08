b44cd9f0b7376e5904744f9030c5a5a84fc7c500
ManiyarasuM <maniyarasu888@gmail.com>
2025-03-08 12:56:46 +0530

    Update Github.cmd

--- Github.cmd
+++ Github.cmd
@@ -7,3 +7,5 @@ git push origin main – Push to GitHub
 Move-Item -Path "C:\Users\Dell\Desktop\Learning Git\Github.cmd" -Destination "C:\Users\Dell\Desktop\Learning Git\Demo"
 git mv old-filename.txt new-filename.txt
 git rm --cached Demo
+we are changed any thing from those file then we need to again follow the same process
+(1.git init, 2.git add <file.name>, 3.git commit -m "your message", 4.git commit origin main)
