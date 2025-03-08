96f3b09bcbb94aa9638c5979a9ac35cc795294f5
ManiyarasuM <maniyarasu888Gmail.com>
2025-03-08 12:52:05 +0530

    file updated

---
+++ Github.cmd
@@ -0,0 +1,9 @@
+Basic Concept:
+git init – Initialize repo
+git remote add origin <URL> – Link remote repo
+git add <filename> – Stage the file
+git commit -m "message" – Commit changes
+git push origin main – Push to GitHub
+Move-Item -Path "C:\Users\Dell\Desktop\Learning Git\Github.cmd" -Destination "C:\Users\Dell\Desktop\Learning Git\Demo"
+git mv old-filename.txt new-filename.txt
+git rm --cached Demo
