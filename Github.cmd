cb67046e2d87698bb34ed04d195a8196c72614e3
ManiyarasuM <maniyarasu888Gmail.com>
2025-03-08 15:36:27 +0530

    added merge concept

--- Github.cmd
+++ Github.cmd
@@ -10,3 +10,6 @@ git rm --cached Demo
 we are changed any thing from those file then we need to again follow the same process
 (1.git init, 2.git add <file.name>, 3.git commit -m "your message", 4.git commit origin main)
 Merge Concept:
+create one file from another Repo and move to another branch
+Example:  first check 1. git branch and create one new branch(git branch branchname) 2. push one file to new branch(git push origin branchname)
+3. check differen(git diff demo) 4. change branch(gitcheckout branchname) 5. merge two diff branch(git merge branchname)
